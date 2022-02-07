9422ec45ee308c751e2ffa2c0806a6b34ea09364
Pascal JK <pascual.santos80@gmail.com>
2022-02-07 14:45:27 +0200

    *Added code to Program Class

--- SimpleConsole/Program.cs
+++ SimpleConsole/Program.cs
@@ -1,3 +1,8 @@
 ﻿using SimpleConsole;
 
+var shapes = new List<Shape>();
+shapes.Add(new Shape({ Height = 100, Width = 30, Type = ShapeType.Circle});
+shapes.Add(new Shape({ Height = 150, Width = 50, Type = ShapeType.Rectangle});
 
+var canvas = new Canvas();
+canvas.DrawShapes(shapes);
